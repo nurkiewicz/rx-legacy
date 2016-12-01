1261a1f1266cc63d23d0b383464f4f2056bece03
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2016-12-01 12:02:32 +0200

    Random sleeps, cosmetics

--- src/test/java/com/nurkiewicz/cache/CacheServer.java
+++ src/test/java/com/nurkiewicz/cache/CacheServer.java
@@ -1,7 +1,12 @@
 package com.nurkiewicz.cache;
 
+import com.nurkiewicz.util.Sleeper;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rx.Observable;
+
+import java.time.Duration;
 
 public class CacheServer {
 
@@ -9,7 +14,13 @@ public class CacheServer {
 
 	public String findBy(long key) {
 		log.info("Loading from Memcached: {}", key);
+		Sleeper.sleep(Duration.ofMillis(RandomUtils.nextInt(100, 200)));
+		log.info("Cache hit for: {}", key);
 		return "<data>" + key + "</data>";
+	}
+
+	public Observable<String> rxFindBy(long key) {
+		return Observable.fromCallable(() -> findBy(key));
 	}
 
 }
--- src/test/java/com/nurkiewicz/weather/WeatherClient.java
+++ src/test/java/com/nurkiewicz/weather/WeatherClient.java
@@ -1,6 +1,7 @@
 package com.nurkiewicz.weather;
 
 import com.nurkiewicz.util.Sleeper;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +13,9 @@ public class WeatherClient {
 
 	public Weather fetch(String city) {
 		log.info("Loading for {}", city);
-		Sleeper.sleep(Duration.ofMillis(900));
+		Sleeper.sleep(Duration.ofMillis(RandomUtils.nextInt(900, 950)));
 		//HTTP, HTTP, HTTP
+		log.info("Done: {}", city);
 		return new Weather();
 	}
 
